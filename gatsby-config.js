eae5c06af122cabaae7d2e62dbd5d7825999c17e
lukewulf <lwulf@ucsd.edu>
2022-01-22 21:11:32 -0800

    Gatsby Tutorial step 3 complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
       title: `Test Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ]
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Greeting from '../components/greeting'
+import { StaticImage } from 'gatsby-plugin-image'
 
 //Step 2: Define component
 const AboutPage = () => {
@@ -10,6 +11,10 @@ const AboutPage = () => {
             <p>Hi there!  I'm the proud creator of this site, which is built upon Gatsby</p>
             <Greeting name="Luke"></Greeting>
             <Greeting name="Marc"></Greeting>
+            <StaticImage 
+                alt="Azuki nft of a flame demon"
+                src="../images/azuki.png">
+            </StaticImage>
         </Layout>
     )
 }
