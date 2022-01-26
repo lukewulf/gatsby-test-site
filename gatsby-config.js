73a44b069c48e57cf8dd8f36dd280a47fffd0323
lukewulf <lwulf@ucsd.edu>
2022-01-26 09:20:50 -0800

    Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,19 @@
 module.exports = {
   siteMetadata: {
-      title: `Test Gatsby Site`,
+      title: `Azuki`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
   ]
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,19 +2,22 @@ import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({data}) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <MDXRenderer>
+                            {node.body}
+                        </MDXRenderer>
+                    </article>
+                ))
+            }
             <StaticImage 
                 alt="another Azuki"
                 src="https://i1.sndcdn.com/artworks-M2WleHThylClTCWD-Ee9tiw-t500x500.jpg">
@@ -25,11 +28,16 @@ const BlogPage = ({data}) => {
 
 export const query = graphql
     `query {
-        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
             nodes {
-                name
+              id
+              frontmatter {
+                date(formatString: "MMMM D, YYYY")
+                title
+              }
+              body
             }
-        }  
+          } 
     }`
 
 export default BlogPage
